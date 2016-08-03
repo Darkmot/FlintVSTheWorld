e485ba0f49009632dee720e3b83fc07577a4da9c
Timotius Prihadi <thedarkmot@gmail.com>
2016-08-03 07:36:55 +0700

    Basic Action

---
+++ Assets/Script/PrototypeLabel.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public enum PLabel {
+	Start,
+	Miss,
+	Bad,
+	Good,
+	Perfect,
+	None
+}
+
+
+public class PrototypeLabel : MonoBehaviour {
+
+	public void ChangeLabel (PLabel pl)
+	{
+		Text t = GetComponent<Text> ();
+		switch (pl) {
+		case PLabel.Start:
+			t.text = "PRESS TO START";
+			t.color = Color.red;
+			break;
+		case PLabel.Miss:
+			t.text = "MISS";
+			t.color = Color.red;
+			break;
+		case PLabel.Bad:
+			t.text = "BAD";
+			t.color = new Color(1.0f,0.35f,0f,1f);
+			break;
+		case PLabel.Good:
+			t.text = "GOOD";
+			t.color = Color.yellow;
+			break;
+		case PLabel.Perfect:
+			t.text = "PERFECT";
+			t.color = Color.green;
+			break;
+		case PLabel.None:
+			t.text = "";
+			break;
+		}
+	}
+
+}
---
+++ Assets/Script/PrototypeManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PGameType {
+	Start,
+	Run,
+	Stop
+}
+
+public class PrototypeManager : MonoBehaviour {
+
+	public PrototypeLabel plabel;
+	public Animator questionHalo;
+	public Animator answerHalo;
+	public PGameType gameType;
+
+	// Use this for initialization
+	void Start () {
+		Restart ();
+	}
+
+	public void Restart() {
+		gameType = PGameType.Start;
+		plabel.ChangeLabel (PLabel.Start);
+	}
+
+	public void Begin() {
+		gameType = PGameType.Run;
+		plabel.ChangeLabel (PLabel.None);
+	}
+
+	public void TapAction() {
+
+		switch (gameType) {
+		case PGameType.Start:
+			Begin ();
+			break;
+		case PGameType.Run:
+			answerHalo.SetTrigger ("Anim");
+			plabel.ChangeLabel (PLabel.Perfect);
+			break;
+		case PGameType.Stop:
+			break;
+		}
+
+	}
+	
+}
