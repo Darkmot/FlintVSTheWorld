32dd30b25284b07b6a978528b4113d04362fac64
Timotius Prihadi <thedarkmot@gmail.com>
2016-08-03 23:14:34 +0700

    Basic Music Randomization and tap

---
+++ Assets/Script/PrototypeAudioLibrary.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class AudioTheme {
+	public string name;
+	public AudioClip intro;
+	public AudioClip[] segments;
+	public AudioClip question;
+}
+
+
+public class PrototypeAudioLibrary : MonoBehaviour {
+
+	public AudioTheme[] audioTheme;
+	public AudioClip answer;
+
+}
--- Assets/Script/PrototypeManager.cs
+++ Assets/Script/PrototypeManager.cs
@@ -14,19 +14,36 @@ public class PrototypeManager : MonoBehaviour {
 	public Animator answerHalo;
 	public PGameType gameType;
 
+	public AudioSource musicSource;
+	public AudioSource questionSource;
+	public AudioSource answerSource;
+	public PrototypeAudioLibrary audioLibrary;
+
+	int selectedTheme;
+	int selectedSong;
+
 	// Use this for initialization
 	void Start () {
 		Restart ();
 	}
 
 	public void Restart() {
+		answerSource.Stop ();
+		musicSource.Stop ();
+		questionSource.Stop ();
+
 		gameType = PGameType.Start;
 		plabel.ChangeLabel (PLabel.Start);
+		answerSource.clip = audioLibrary.answer;
+		selectedTheme = Random.Range (0,audioLibrary.audioTheme.Length);
+		musicSource.clip = audioLibrary.audioTheme [selectedTheme].intro;
+		questionSource.clip = audioLibrary.audioTheme [selectedTheme].question;
 	}
 
 	public void Begin() {
 		gameType = PGameType.Run;
 		plabel.ChangeLabel (PLabel.None);
+		musicSource.Play ();
 	}
 
 	public void TapAction() {
@@ -38,11 +55,22 @@ public class PrototypeManager : MonoBehaviour {
 		case PGameType.Run:
 			answerHalo.SetTrigger ("Anim");
 			plabel.ChangeLabel (PLabel.Perfect);
+			answerSource.Play ();
 			break;
 		case PGameType.Stop:
 			break;
 		}
 
 	}
-	
+
+	void RandomizeSong() {
+		selectedSong = Random.Range (0,audioLibrary.audioTheme[selectedTheme].segments.Length);
+		musicSource.clip = audioLibrary.audioTheme [selectedTheme].segments[selectedSong];
+		musicSource.Play ();
+	}
+
+	void Update() {
+		if ((gameType==PGameType.Run) && (!musicSource.isPlaying))
+			RandomizeSong ();
+	}
 }
